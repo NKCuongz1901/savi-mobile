19f2e338b83c4c4cdf009d2b4787b11d11e3657f
nkhaccuong <cuong19012002@gmail.com>
2025-09-22 23:54:21 +0700

    chore: init project

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold color-red-400">Hello World 2</Text>
+    </View>
+  );
+}
